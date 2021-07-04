948029caf88358b0914a83cc32c3d054a9b65624
LukeMataika <78513611+LukeMataika@users.noreply.github.com>
2021-07-05 10:55:30 +1200

    variable cost and fc base

--- 00_FC_Base_v1.py
+++ 00_FC_Base_v1.py
@@ -1,11 +1,14 @@
-#import libraries
 
-# *** Function Goes Here ***
+# import libraries 
 
 
+# *** Functions go here ***
+
+# checks that input is either a float or an
+# integer that is more than zero. takes in custom error message
 def num_check(question, error, num_type):
     valid = False
-    while not valid :
+    while not valid:
 
         try:
             response = num_type(input(question))
@@ -18,10 +21,63 @@ def num_check(question, error, num_type):
         except ValueError:
             print(error)
 
-# main routine goes here
-get_int = num_check("how many do you need? ",
-                    "please enter an amount more than 0\n",
+# Checks user enters valid choice based on a list
+def string_check(question, options):
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in options:
+
+            # check if response is in the list
+            if response == var_item:
+                return var_item
+            
+            # if response is the first letter of something in the list, return the entire word
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter a valid response")
+ 
+ #  *** Main routine starts here *****
+
+
+# decorates statements to make them stand out...
+def statement_generator(statement, decoration, amount):
+
+    multi_dec = decoration * amount
+
+    print("{} {} {}".format(multi_dec, statement, multi_dec))
+
+    return ""
+
+# Main routine goes here
+
+# lists go here...
+yes_no_list = ["yes", "no"]
+
+
+# ask user if they need instructions...
+
+statement_generator("Fund Raising Calculator", "*", 6)
+print()
+
+want_help = string_check("Do you want to read the instructions? ", yes_no_list)
+
+if want_help == "yes":
+    print("Useful instructions go here")
+
+print()
+statement_generator("Lets get started", "-", 3)
+print()
+
+get_int = num_check("How many do you need? ",
+                    "PLease enter an amount more than 0\n",
                     int)
-get_cost = num_check("how much does it cost? $",
-                    "please enter a number more than 0\n",
+get_cost = num_check("How much does it cost? $",
+                    "Please enter a number more than 0\n",
                     float)
+
+print("You need: {}".format(get_int))
